42314f72b53fa1b556a2257ed6bdda5de429de50
Johnny <delga300@hotmail.com>
2022-11-24 20:14:16 -0300

    Primeiro commit do bot

---
+++ src/test/java/br/com/bot/botapi/BotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bot.botapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
