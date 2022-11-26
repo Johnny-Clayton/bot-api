e906dda131bd943ccea337d756c5e12042044bf7
Johnny <delga300@hotmail.com>
2022-11-26 18:41:46 -0300

    Criação da estrutura para pegar o tempo da partida

--- src/main/java/br/com/bot/botapi/util/ScrapingUtil.java
+++ src/main/java/br/com/bot/botapi/util/ScrapingUtil.java
@@ -1,6 +1,7 @@
 package br.com.bot.botapi.util;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -18,11 +19,22 @@ public class ScrapingUtil {
 	
 	public static void main(String[] args) {
 
-		String complemento = "";
-		String url = "https://cursos.dankicode.com/login"; //BASE_URL_GOOGLE + complemento; 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informa a o jogo: ");
+		String complemento = sc.nextLine().replace(" ", "+");
+				
+//				"argentina+x+méxico";
+		
+		String url = "https://www.google.com/search?q=" + complemento;
+//		String url = "https://www.google.com/search?q=tun%C3%ADsia+x+austr%C3%A1lia";  
+//		String url = BASE_URL_GOOGLE + complemento; 
 		
 		ScrapingUtil scraping = new ScrapingUtil();
 		scraping.obtemInformacoesPartida(url);
+		
+		PartidaGoogleDTO par = new PartidaGoogleDTO();
+		
 	}
 	
 	public PartidaGoogleDTO obtemInformacoesPartida(String url) {
@@ -36,11 +48,59 @@ public class ScrapingUtil {
 			String title = document.title();
 			LOGGER.info("Titulo da pagína: {}", title);
 			
+			String partidaStatus = obtemStatusPartida(document).toString();
+			String partidaTempo = obtemTempoPartida(document);
+			
+//			LOGGER.info("Status: {}", partid);
+			
 		} catch (IOException e) {
 			LOGGER.error("ERRO AO CONECTAR NA URL COM JSOUP -> {}", e.getMessage());
 		}
 		
 		return partida;
+	}
+	
+	public StatusPartida obtemStatusPartida(Document document) {
+		
+		StatusPartida statusPartida = StatusPartida.PARTIDA_NAO_INICIADA;
+		
+		boolean isTempoPardida = document.select("div[class=imso_mh__lv-m-stts-cont]").isEmpty();
+		
+		if(!isTempoPardida) {
+			String tempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").first().text();
+			statusPartida = StatusPartida.PARTIDA_EM_ANDAMENTO;
+			if(tempoPartida.contains("Pênaltis")) {
+				statusPartida = StatusPartida.PARTIDA_PENATIS;
+			}
+			LOGGER.info(tempoPartida);
+		}
+		
+		isTempoPardida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").isEmpty();
+		
+		if(!isTempoPardida) {
+			statusPartida = StatusPartida.PARTIDA_ENCERRADA;
+		}
+		LOGGER.info(statusPartida.toString());
+		return statusPartida;
+
+	}
+	
+	public String obtemTempoPartida(Document document) {
+		
+		String tempoPartida = null;
+		
+		boolean isTempoPardida = document.select("div[class=imso_mh__lv-m-stts-cont]").isEmpty();
+		
+		if(!isTempoPardida) {
+			tempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").first().text();
+		}
+		
+		isTempoPardida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").isEmpty();
+		if(!isTempoPardida) {
+			tempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").first().text();
+		}
+		LOGGER.info(tempoPartida);
+		return tempoPartida.replace("'", "min");
 	}
 
 }
